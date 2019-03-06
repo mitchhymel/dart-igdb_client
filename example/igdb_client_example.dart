cf6a575f2ff29d2b67418e62707d96db09cebfcb
mitchhymel <mjhymel111@gmail.com>
2019-03-05 16:32:18 -0800

    Updating readme and example

--- example/igdb_client_example.dart
+++ example/igdb_client_example.dart
@@ -1,6 +1,6 @@
-import 'package:igdb_client/igdb_client.dart';
-
 import 'api_key.dart';
+
+import 'package:igdb_client/igdb_client.dart';
 
 var client = new IGDBClient(
     MY_USER_AGENT,
@@ -9,17 +9,29 @@ var client = new IGDBClient(
 
 main() async {
 
-  
-  var gamesResponse = await client.games(IGDBRequestParameters(
-    limit: 5,
-    fields: ['name', 'rating', 'time_to_beat', 'release_dates'],
-    order: 'rating desc',
+
+  // Find games with 'infamous' in their name and return
+  // the results' name and expand their release_dates and platforms.
+  var gamesResponse = await client.games(new IGDBRequestParameters(
+    search: 'infamous',
+    fields: ['name', 'release_dates.*', 'platforms.*']
   ));
+  printResponse(gamesResponse);
 
-  print(IGDBClient.getPrettyStringFromMap(gamesResponse.toMap()));
+  // Find games that are not yet released but are releasing soon in the
+  // North American, Worlwide, or Unspecified regions, then sort by 
+  // release date.
+  int msecSinceEpoch = DateTime.now().millisecondsSinceEpoch;
+  int secsSinceEpoch = msecSinceEpoch~/1000;
+  String timeNow = secsSinceEpoch.toString();
+  var releaseResponse = await client.releaseDates(new IGDBRequestParameters(
+    filters: 'date > $timeNow & (region = ${IGDBRegions.NORTH_AMERICA.id} | region = ${IGDBRegions.NONE.id} | region = ${IGDBRegions.WORLDWIDE.id})',
+    order: 'date asc'
+  ));
+  printResponse(releaseResponse);
+}
 
-  
-
-  // print(response.toPrettyString());
+printResponse(IGDBResponse resp) {
+  print(IGDBClient.getPrettyStringFromMap(resp.toMap()));
 }
 
