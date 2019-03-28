44335ba5ae6cef093def7de53b3bda52df50f7d0
Giovanni Lattanzio <gilattanzio@gmail.com>
2019-03-28 15:45:28 +0100

    Managing alpha channel on getImageUrl

--- lib/src/igdb_client.dart
+++ lib/src/igdb_client.dart
@@ -192,13 +192,15 @@ class IGDBClient {
 
   /**
    * Helper method to get the igdb url to show an image provided
-   * its [imageId] and [size]. Provide [isRetina] if you want to double
-   * the image resolution.
+   * its [imageId] and [size].
+   * Provide [isRetina] if you want to double the image resolution and
+   * [alphaChannel] if you know the image has an alpha in it (avoiding black background, for example).
    */
   static String getImageUrl(String imageId, IGDBImageSizes size,
-      {bool isRetina=false}) {
+      {bool isRetina=false, bool alphaChannel=false}) {
     String sizeStr = isRetina ? '${size.name}_2x' : '${size.name}';
-    return 'https://images.igdb.com/igdb/image/upload/t_${sizeStr}/${imageId}.jpg';
+    String fileExtension = alphaChannel ? 'png' : 'jpg';
+    return 'https://images.igdb.com/igdb/image/upload/t_${sizeStr}/${imageId}.$fileExtension';
   }
 
   /**
